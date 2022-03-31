b80a242d5aa587ceacd8a978cd5fd8df0512d7e8
Max Mezalon <jmezalon@gmail.com>
2022-03-31 15:03:15 -0400

    added meeting controllers to match their respective endpoints

--- app/controllers/api/meetings_controller.rb
+++ app/controllers/api/meetings_controller.rb
@@ -1,2 +1,30 @@
 class Api::MeetingsController < ApplicationController
+    before_action :find_room, only: [:show, :show_bookings]
+
+    def index
+        render json: Meeting.all, status: :ok
+    end
+
+    def create
+        meeting = Meeting.create(meeting_params)
+        render json: meeting, status: :created
+    end
+
+    def show
+        render json: @room, status: :ok
+    end
+
+    def show_bookings
+        render json: @room.bookings, status: :ok
+    end
+
+    private  
+
+    def meeting_params 
+        params.permit(:meeting_name, :capacity, :floor)
+    end
+
+    def find_room
+        @room = Meeting.find(params[:id])
+    end
 end
