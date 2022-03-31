fa36653bc3cfc012c5d41f9a089ef697ea26b02a
Max Mezalon <jmezalon@gmail.com>
2022-03-31 15:12:43 -0400

    added booking controllers to match their respective endpoints

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,2 +1,30 @@
 class Api::BookingsController < ApplicationController
+    before_action :find_booking, only: [:show, :destroy]
+
+    def index
+        render json: Booking.all, status: :ok
+    end
+
+    def show
+        render json: @booking, status: :ok
+    end
+
+    def create
+        render json: Booking.create(booking_params), status: :created
+    end
+
+    def destroy 
+        @booking.destroy
+        head :no_content
+    end
+
+    private 
+
+    def booking_params
+        params.permit(:meeting_name, :meeting_id, :start_date, :end_date, :attendees)
+    end
+
+    def find_booking
+        @booking = Booking.find(params[:id])
+    end
 end
