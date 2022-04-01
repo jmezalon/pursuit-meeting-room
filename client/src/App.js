46ec7f3554004f351cfef454a74add1f0ee16b87
Max Mezalon <jmezalon@gmail.com>
2022-04-01 14:24:47 -0400

    started css for app container

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>starting</h1>
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="home-container">
+      <header className="page-indication">
+        <h1>Home</h1>
+      </header>
+      <main className="navbar-body-container">
+        <p>will have navbar here</p>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
